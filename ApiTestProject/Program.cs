a286aebbf7c4d3043df9be6f9185fd910aa02f2f
unknown <fernando_ho93@hotmail.com>
2024-10-10 16:56:03 -0300

    alterações no projeto de estudos de API - HeadTest e GetWithQueryTest

--- ApiTestProject/Program.cs
+++ ApiTestProject/Program.cs
@@ -7,18 +7,17 @@ namespace ApiTestProject
     {
         static void Main(string[] args)
         {
-            // Descomente a função que deseja executar
+            //Estudos de teste de API com c# e Selenium
+            // Comente as funções que não serão utilizadas
+
             GetTest();
-            //PostTest();
-            //PutTest();
-            // DeleteTest();
-            // HeadTest();
-            // GetWithQueryTest();
+            PostTest();
+            PutTest();
+            DeleteTest();
+            HeadTest();
+            GetWithQueryTest();
         }
 
-        /// <summary>
-        /// Exemplo de como fazer um GET para pegar todos os posts
-        /// </summary>
         static void GetTest()
         {
             var client = new RestClient("https://jsonplaceholder.typicode.com");
@@ -53,14 +52,14 @@ namespace ApiTestProject
 
         static void PutTest(){
             var client = new RestClient("https://jsonplaceholder.typicode.com");
-            var request = new RestRequest("/posts/1", Method.Put);
+            var request = new RestRequest("/posts/100", Method.Put);
 
             request.AddJsonBody(new
             {
-                id = 1, 
+                id = 10, 
                 title = "updated title",
                 body = "updated body",
-                userId = 1
+                userId = 100
             });
 
             RestResponse response = client.Execute(request);
@@ -75,6 +74,57 @@ namespace ApiTestProject
                 Console.WriteLine("Erro ao atualizar recurso: " + response.StatusDescription);
             }
         }
-        // Adicione as outras funções aqui...
+        
+        static void DeleteTest(){
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/posts/1000", Method.Delete);
+
+            RestResponse response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine("Recurso deletado com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("Erro ao deletar recurso: " + response.StatusDescription);
+            }
+        }
+
+        static void HeadTest(){
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/posts", Method.Head);
+            RestResponse response = client.Execute(request);
+
+            foreach (var header in response.Headers)
+            {
+                Console.WriteLine($"{header.Name}: {header.Value}");
+            }
+            
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine("Requisição Head bem sucedida!");
+            }
+            else
+            {
+                Console.WriteLine("Erro na requisição Head: " + response.StatusDescription);
+            }
+        }    
+        static void GetWithQueryTest(){
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var request = new RestRequest("/posts", Method.Get);
+            request.AddParameter("userId", 1);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
+            if (response.IsSuccessful)
+            {
+                Console.WriteLine("Requisição GET com parâmetros bem-sucedida!");
+            }
+            else
+            {
+                Console.WriteLine("Erro na requisição GET com parâmetros: " + response.StatusDescription);
+            }
+        }    
     }
 }
